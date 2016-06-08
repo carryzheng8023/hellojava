be8bfbd9fa1647937556cc1dd09131384d3830c0
郑鑫 <zhengxin@syswin.com>
2016-06-08 09:57:49 +0800

    修复递归二分查找栈溢出的bug

--- src/main/java/com/zhengxin/algorithm/Search.java
+++ src/main/java/com/zhengxin/algorithm/Search.java
@@ -1,11 +1,25 @@
 package com.zhengxin.algorithm;
 
+import java.util.Arrays;
 import java.util.List;
+import static com.zhengxin.j2se.net.mindview.util.Print.print;
 
 /**
  * Created by zhengxin on 16/6/7.
  */
 public class Search {
+
+    public static void main(String[] a){
+
+        List<Integer> numbers = Arrays.asList(1,5,7,7,8);
+
+        print("数组：" + numbers);
+        print("5是否在数组中：" + Search.binarySearch(numbers, 5));
+        print("10是否在数组中：" + Search.binarySearch(numbers, 10));
+
+
+    }
+
 
     public static boolean binarySearch(final List<Integer> numbers, final Integer value){
 
@@ -13,6 +27,9 @@ public class Search {
             return false;
 
         final Integer comparsion = numbers.get(numbers.size() / 2);
+
+        if(numbers.size() == 1 && !value.equals(comparsion))
+            return false;
 
         if(value.equals(comparsion))
             return true;
