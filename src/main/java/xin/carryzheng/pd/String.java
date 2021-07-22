829c41194aa10c36c28a20fba9071faba8d0a963
zhengxin <carryzheng8023@gmail.com>
2021-07-23 04:23:35 +0800

    classloader 双亲委派

---
+++ src/main/java/xin/carryzheng/pd/String.java
@@ -0,0 +1,11 @@
+package xin.carryzheng.pd;
+
+/**
+ * @author zhengxin
+ * @date 2021-06-10 19:16:20
+ */
+public class String {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
